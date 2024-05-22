baed40930331292169c1370f306f1587e1230882
root <root@cubeprod-in.unicommerce.infra>
2024-05-22 17:01:37 +0530

    Daily Backup

--- backup.sh
+++ backup.sh
@@ -16,4 +16,4 @@ git add .
 git commit -m "$COMMIT_MESSAGE"
 
 # Push changes to the specified branch
-git push https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/$(REPO).git "$BRANCH"
+git push https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/${REPO}.git "$BRANCH"
